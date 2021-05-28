43bdf7423d85c8b48a30f3dbf5b05d6ba112150a
Shivansh <shivanshpahwa24@gmail.com>
2021-05-28 20:47:48 +0530

    API part done

--- routes/api/branches.js
+++ routes/api/branches.js
@@ -15,94 +15,45 @@ router.get("/banks", async (req, res) => {
   }
 });
 
-// Get all branches
-router.get(`?q=:branchCity&limit=:limit&offset=:offset`, async (req, res) => {
+// Get branches based on possible matches across all columns and all rows
+router.get("/", async (req, res) => {
   try {
+    let { q, limit, offset } = req.query;
+    q = q.toUpperCase();
+    console.log(q);
+
     const branches = await db.query(
-      `SELECT * FROM branches where city=${req.params.branchCity}`
+      `
+      SELECT * FROM branches 
+      WHERE branch LIKE '%${q}%' OR address LIKE '%${q}%' OR city LIKE '%${q}%' OR district LIKE '%${q}%' OR state LIKE '%${q}%' 
+      ORDER BY ifsc
+      LIMIT ${limit} OFFSET ${offset}
+      `
     );
     res.json(branches.rows);
   } catch (err) {
     console.error(err.message);
   }
 });
-/* 
-// Add a user marks
-router.post(
-  "/",
-  [
-    check("rollNo", "Roll No. is required").not().isEmpty(),
-    check("name", "Name is required").not().isEmpty(),
-    check("maths")
-      .not()
-      .isEmpty()
-      .withMessage("Maths Marks are required")
-      .bail()
-      .isNumeric()
-      .withMessage("Maths Marks should be a number")
-      .bail()
-      .isInt({ min: 0, max: 100 })
-      .withMessage("Maths Marks should be between 0 and 100"),
-    check("physics")
-      .not()
-      .isEmpty()
-      .withMessage("Physics Marks are required")
-      .bail()
-      .isNumeric()
-      .withMessage("Physics Marks should be a number")
-      .bail()
-      .isInt({ min: 0, max: 100 })
-      .withMessage("Physics Marks should be between 0 and 100"),
-    check("chemistry")
-      .not()
-      .isEmpty()
-      .withMessage("Chemistry Marks are required")
-      .bail()
-      .isNumeric()
-      .withMessage("Chemistry Marks should be a number")
-      .bail()
-      .isInt({ min: 0, max: 100 })
-      .withMessage("Chemistry Marks should be between 0 and 100"),
-  ],
-  async (req, res) => {
-    const errors = validationResult(req); //Check for any errors
-    if (!errors.isEmpty()) {
-      return res.status(400).json({ errors: errors.array() });
-    }
 
-    let { rollNo, name, maths, physics, chemistry } = req.body;
+// Get branches based on branch name AUTOCOMPLETE API
+router.get("/autocomplete", async (req, res) => {
+  try {
+    let { q, limit, offset } = req.query;
+    q = q.toUpperCase();
 
-    const total = parseInt(maths) + parseInt(physics) + parseInt(chemistry);
-    const percentage = (total / 3).toFixed(2) + "%";
-    const id = uuid();
-
-    try {
-      //See if User exists
-      let user = await User.findOne({ where: { rollNo } });
-      if (user) {
-        return res
-          .status(400)
-          .json({ errors: [{ msg: "Roll No. already exists" }] });
-      }
-
-      // Insert into table
-      user = await User.create({
-        id,
-        rollNo,
-        name,
-        maths,
-        physics,
-        chemistry,
-        total,
-        percentage,
-      });
-
-      res.json(user);
-    } catch (err) {
-      console.error(err.message);
-      res.status(500).send("Server Error");
-    }
+    const branches = await db.query(
+      `
+      SELECT * FROM branches 
+      WHERE branch LIKE '%${q}%' 
+      ORDER BY ifsc
+      LIMIT ${limit} OFFSET ${offset}
+      `
+    );
+    res.json(branches.rows);
+  } catch (err) {
+    console.error(err.message);
   }
-);
- */
+});
+
 module.exports = router;
